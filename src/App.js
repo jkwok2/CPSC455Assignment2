c1e875914fc4476e2a845b0cb5d800e2013c7cd1
UBC Student <47469240+jkwok2@users.noreply.github.com>
2021-06-07 02:59:00 -0700

    attempted to fix form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Form from "./components/Form"
+import {Form} from "./components/Form"
+import {Card} from "./components/Card"
+import {List} from "./components/List"
+import {Body} from "./components/Body"
 
 function App() {
   return (
     <div className="App">
-      <Form/>
+      <Body/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function Form() {
+export function Form() {
     return (
         <form class="form">
-            <label htmlFor="name">Name</label><br />
+            <label>Name</label><br />
             <input id="imgname" type="text" name="name" /><br />
-            <label htmlFor="url">Image URL</label><br />
+            <label>Image URL</label><br />
             <input id="imgurl" type="text" name="url" />
             <input id="imgsubmit" type="submit" defaultValue="Submit" />
             <input type="reset" defaultValue="Clear" />
         </form>
     );
 }
-
-export default Form;
