06aa6b94cbae4e2a4f960b949fdd6fd053f59fc3
UBC Student <47469240+jkwok2@users.noreply.github.com>
2021-06-21 18:12:05 -0700

    added separate data file

---
+++ be/public/data.js
@@ -0,0 +1,12 @@
+let cardlist = [
+    {
+        name: "maya",
+        url: "https://www.usmagazine.com/wp-content/uploads/2018/11/Maya-The-Dog-3.jpg?w=700&quality=86&strip=all"
+    },
+    {
+        name: "tucker",
+        url: "https://yt3.ggpht.com/ytc/AAUvwnhbv9F6Du9P6GVUYfap8qBfe2_nUGqH6xm3HE9C3Q=s900-c-k-c0x00ffffff-no-rj"
+    }
+]
+
+module.exports = data;
--- be/routes/cardlist.js
+++ be/routes/cardlist.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const data = require("../public/data")
 
-let cardlist = [
-    {
-        name: "maya",
-        url: "https://www.usmagazine.com/wp-content/uploads/2018/11/Maya-The-Dog-3.jpg?w=700&quality=86&strip=all"
-    },
-    {
-        name: "tucker",
-        url: "https://yt3.ggpht.com/ytc/AAUvwnhbv9F6Du9P6GVUYfap8qBfe2_nUGqH6xm3HE9C3Q=s900-c-k-c0x00ffffff-no-rj"
-    }
-]
+router.use(express.json());
+router.use(express.urlencoded({ extended: false }));
 
-router.get('/', (req, res, next) => {
-    res.send(cardlist);
+router.get('/', (req, res) => {
+    res.send(data);
+})
+
+router.post('/', (req, res) => {
+    res.send("foo");
 })
 
 module.exports = router;
