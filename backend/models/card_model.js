185336c6b6696fe3784633fd2e61dce365386cae
UBC Student <47469240+jkwok2@users.noreply.github.com>
2021-07-26 17:14:33 -0700

    removed backend folder

--- backend/models/card_model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const cardSchema = new mongoose.Schema({
-    url: String,
-    name: String,
-    cuteness: Number,
-  });
-
-  module.exports = cardSchema;
