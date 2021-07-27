278fa058506b4ecc4f3041bb8797db2ffc694d86
UBC Student <47469240+jkwok2@users.noreply.github.com>
2021-07-26 18:16:55 -0700

    changed package.json

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, './client/build/index.html'))
 });
 app.use('/users', usersRouter);
 app.use('/cardlist', cardlistRouter)
